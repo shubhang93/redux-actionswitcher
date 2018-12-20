dbeca59ae826da34c7a815abbc9758cae6bff2ae
shubhangb <shubhang@channelbridge.in>
2018-12-20 17:40:52 +0530

    add function

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//@flow
+const actionSwitcher = function(actions, initialState) {
+  return function(state = initialState, action) {
+    if (action && actions.hasOwnProperty(action.type))
+      return actions[action.type].call(null, state, action);
+    return state;
+  };
+};
+
+export default actionSwitcher;
